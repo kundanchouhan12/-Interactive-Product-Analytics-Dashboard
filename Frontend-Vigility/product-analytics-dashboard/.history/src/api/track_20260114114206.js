023214e61edf5ef5222d096f53b52ec225698ab7
kundansinghchouhan <kundansinghchouhan198@gmail.com>
2026-01-18 19:52:55 +0530

    Remove .history folder from git

--- Frontend-Vigility/product-analytics-dashboard/.history/src/api/track_20260114114206.js
+++
@@ -1,5 +0,0 @@
-import api from "./axios";
-
-export const trackEvent = (featureName) => {
-  return api.post(`/track?featureName=${featureName}`);
-};
--- Frontend-Vigility/product-analytics-dashboard/.history/src/api/track_20260114150313.js
+++
@@ -1,9 +0,0 @@
-import API from "./axios";
-
-export const trackEvent = async (featureName) => {
-  try {
-    await API.post(`/track?featureName=${featureName}`);
-  } catch (err) {
-    console.error("Tracking failed:", err);
-  }
-};
